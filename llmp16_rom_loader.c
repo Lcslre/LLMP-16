00466517cc73438ea0a0f868d0eaa4efb91f12a8
Lcslre <210505588+Lcslre@users.noreply.github.com>
2025-05-14 11:52:44 +0200

    réservation première page ROM

--- llmp16_rom_loader.c
+++ llmp16_rom_loader.c
@@ -9,11 +9,12 @@ void llmp16_rom_load(llmp16_t *cpu, char* file){
 	fread(&head, sizeof(llmp16_header_rom_file_t), 1, rom_file);
 	if(head.code != FILE_CODE)
 		return;
-	int page_act = 0;
+	//page 0 réservé au bios et interuptions
+	int page_act = 1;
 	while(page_act < head.nb_pages){
   		char* page_content = (char*)malloc(sizeof(head.taille_page[head.nb_pages]));
   		fread(page_content, sizeof(head.taille_page[head.nb_pages]), 1, rom_file);
-		memcpy( cpu->ROM[page_act], page_content, sizeof(head.taille_page[head.nb_pages]));
+		memcpy( cpu->ROM[page_act-1], page_content, sizeof(head.taille_page[head.nb_pages]));
 		free(page_content);
   		page_act++;
 	}
