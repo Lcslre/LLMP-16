a55eb4938c4b8b1885294439b6854c24024975be
Lcslre <lupoke66@gmail.com>
2025-05-21 09:59:33 +0200

    Mes modifs

--- llmp16_rom_loader.c
+++ llmp16_rom_loader.c
@@ -10,13 +10,15 @@ void llmp16_rom_load(llmp16_t *cpu, char* file){
 	if(head.code != FILE_CODE)
 		return;
 	//page 0 réservé au bios et interuptions
-	int page_act = 1;
-	while(page_act < head.nb_pages){
-  		char* page_content = (char*)malloc(sizeof(head.taille_page[head.nb_pages]));
+	
+	while(!(EOF)){
+  		/*char* page_content = (char*)malloc(sizeof(head.taille_page[head.nb_pages]));
   		fread(page_content, sizeof(head.taille_page[head.nb_pages]), 1, rom_file);
 		memcpy( cpu->ROM[page_act-1], page_content, sizeof(head.taille_page[head.nb_pages]));
 		free(page_content);
-  		page_act++;
+  		*/
+
+		/*J'AI PAS COMPRIS */
 	}
 	fclose(rom_file);
 }
